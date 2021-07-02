8608e641a1b4b146caf5baa38ebead2e9826730f
Vikgau10 <singhvikalp2010@gmail.com>
2021-07-02 23:51:49 +0530

    landing page for acer

---
+++ AcerLandingPage/index.js
@@ -0,0 +1,12 @@
+var NavSlide=()=>{
+var burger=document.querySelector('.burger');
+var nav=document.querySelector('.nav-links');
+
+burger.addEventListener('click',()=>{ 
+  
+nav.classList.toggle('.nav-active'); 
+
+});
+
+}
+
